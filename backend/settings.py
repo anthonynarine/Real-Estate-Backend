39e607c489acf22e9a0154b28d4b5697015afcb9
anthonynarine <anarine83@gmail.com>
2023-01-24 13:22:57 -0500

    CORS headers and MiddleWare added + CORS_ALLOWED_ORIGINS added

--- backend/settings.py
+++ backend/settings.py
@@ -48,9 +48,16 @@ INSTALLED_APPS = [
     'corsheaders', 
 ]
 
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:3000",
+
+]
+
+#cors middle ware added 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
