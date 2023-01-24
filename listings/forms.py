a8e3d3dcd646930a7c527e0fbf6828c3e1bcf7ce
anthonynarine <anarine83@gmail.com>
2023-01-24 00:24:45 -0500

    Form Notes Updated

--- listings/forms.py
+++ listings/forms.py
@@ -4,15 +4,16 @@ from django.contrib.gis.geos import Point
 
 #       NOTE  this from must be registered in admin.py
 
+
 class ListingsForm(forms.ModelForm):
     class Meta:
         model = Listing
         fields = "__all__"
         extra_fields = ["latitude", "longitude"]
-        
+
     latitude = forms.FloatField()
     longitude = forms.FloatField()
-        
+
 # CLEAN Method allows us to perform some logic and manipulate the form
 # The super() method allows us access to the form itself.
 # Data is a dictionalry with key:value pair we extract into
@@ -23,60 +24,50 @@ class ListingsForm(forms.ModelForm):
         longitude = data.pop("longitude")
         data["location"] = Point(latitude, longitude, srid=4326)
         return data
-    
-#   NOTE this code will overide the init form method without the overide 
-#        the entered latitude and longitude in the forms will disapear 
-#        The location will be marked on the map but we want to show the 
-#         entered latitude and longitude.  
+
+#   NOTE this code will overide the init form method without the overide
+#        the entered latitude and longitude in the forms will disapear
+#        The location will be marked on the map but we want to show the
+#         entered latitude and longitude.
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         location = self.initial.get("location")
         if isinstance(location, Point):
             self.initial["latitude"] = location.tuple[0]
             self.initial["longitude"] = location.tuple[1]
-# The super() method allows us acces to the form. the super method
+
+# The super() method allows  acces to the form. the super method
 # is initialized with the .__init__(...)
-# Location is Point field and point fields have a property called tuple
-# which returns the coordinates of a Point as a tuple
+# Location is a Point field and point fields have a property called tuple
+# tuple returns the coordinates of a Point as a tuple
 # The if statement check if location is an instance if thats the case
-# we return the logic below.
-    
-    
+# we return latitiude and logitude from their index as show
 
-
-
-
-
-
-#                             "__all__" magic method explained
-            # NOTE this list of fields from Listing is easily added with "__al__" 
+            # "__all__" magic method explained
+            # NOTE this list of fields from Listing is easily added with "__al__"
             # magic method
         # fields = ["title", "description", "area", "listing_type",
         #           "property_status", "price", "rental_frequency",
         #           "rooms", "furnished", "pool", "elevator", "parking",
         #           "date_posted", "location"]
-        
-        
+
         # NOTE the syntax above to add additional fields
-        
-        
+
         # NOTE Clean method allows us to perform some logic and manipulate the form
         #         def clean(self):
         #             data = super().clean
-        #   The super() method allows access to the form itself the 
+        #   The super() method allows access to the form itself the
         #   The data variable will be dictionary with key:value pairs
         #   We needed the location field from the latitude and longitude
         #   fields + it has to be a point object
-        
-        
-        
+
         #                   NOTE SRID
-        # Every geometric shape has a spatial reference system associated 
-        # with it, and each such reference system has a Spatial Reference 
-        # System ID (SRID). The SRID is used to tell which spatial reference 
+        # Every geometric shape has a spatial reference system associated
+        # with it, and each such reference system has a Spatial Reference
+        # System ID (SRID). The SRID is used to tell which spatial reference
         # system will be used to interpret each spatial object.
 
         # A common SRID in use is 4326, which represents spatial data
-        # using longitude and latitude coordinates on the Earth's surface 
-        # as defined in the WGS84 standard, which is also used for the Global 
+        # using longitude and latitude coordinates on the Earth's surface
+        # as defined in the WGS84 standard, which is also used for the Global
         # Positioning System (GPS)
